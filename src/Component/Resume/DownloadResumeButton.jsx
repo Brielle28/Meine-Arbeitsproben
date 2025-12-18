7cbd9be2ccb8480bcdb5db72071f5502330d7bed
Brielle28 <chukwuemerieclara@gmail.com>
2025-12-18 02:30:07 +0100

    resume update

--- src/Component/Resume/DownloadResumeButton.jsx
+++ src/Component/Resume/DownloadResumeButton.jsx
@@ -4,21 +4,15 @@ const DownloadResumeButton = () => {
   const handleDownload = () => {
     // Create a link to the PDF file in the public folder
     const link = document.createElement("a");
-    link.href = "/MyCV/Nnadozie__Clara_-_Frontend_Software_Engineer.pdf"; // Path relative to public folder
-    link.download = "Nnadozie__Clara_-_Frontend_Software_Engineer.pdf";
+    link.href = "/MyCV/Clara_Nnadozie_-_Software_Engineer.pdf"; // Path relative to public folder
+    link.download = "Clara_Nnadozie_-_Software_Engineer.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    // <button
-    //   onClick={handleDownload}
-    //   className="flex items-center gap-2 px-4 py-2 bg-pink-500 hover:bg-pink-600 text-white rounded-lg transition-colors duration-300 font-medium"
-    // >
-    //   <FaDownload />
-    //   <span>Download Resume</span>
-    // </button>
+    
     <button
       onClick={handleDownload}
       className="flex items-center justify-center gap-1 border-[1px] border-gray-300  rounded-[30px] text-gray-500 bg-transparent px-3 py-2"
